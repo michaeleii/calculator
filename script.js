f16b41845ba7fc077b3d658213eff77a9bfc6aee
Michael Lei <michaelleie0ca@gmail.com>
2022-02-08 14:32:37 -0800

    Added display for calculator

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ function operate (operator, num1, num2) {
     return operator(num1, num2);
 }
 
+
+const displayText = document.querySelector("div.display-text");
+const numberButton = document.querySelectorAll("button.numbers");
+
+for (let i = 0; i < numberButton.length; i++) {
+    numberButton[i].addEventListener("click", function () {
+        if (displayText.innerHTML.length <= 20) {
+            displayText.innerHTML += this.innerHTML;
+        };
+    })
+    
+}
