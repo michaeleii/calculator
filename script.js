be58c24ac3d4dd0ff9ca93ba7f47d9e2263c5c47
Michael Lei <michaelleie0ca@gmail.com>
2022-02-08 17:42:28 -0800

    Able to delete display entries

--- script.js
+++ script.js
@@ -21,12 +21,73 @@ function operate (operator, num1, num2) {
 
 const displayText = document.querySelector("div.display-text");
 const numberButton = document.querySelectorAll("button.numbers");
+const operatorButton = document.querySelectorAll("button.operator")
 
 for (let i = 0; i < numberButton.length; i++) {
     numberButton[i].addEventListener("click", function () {
-        if (displayText.innerHTML.length <= 20) {
+        if (displayText.innerHTML.length < 15) {
             displayText.innerHTML += this.innerHTML;
         };
     })
-    
 }
+
+for (let i = 0; i < numberButton.length; i++) {
+    numberButton[i].addEventListener("click", function () {
+        if (displayText.innerHTML.length < 15) {
+            displayText.innerHTML += this.innerHTML;
+        };
+    })
+}
+
+document.addEventListener("keydown", function (event) {
+    if (displayText.innerHTML.length < 15) {
+        switch (event.key) {
+
+            case '1':
+                displayText.innerHTML += "1";
+                break;
+
+            case '2':
+                displayText.innerHTML += "2";
+                break;
+                
+            case '3':
+                displayText.innerHTML += "3";
+                break; 
+
+            case '4':
+                displayText.innerHTML += "4";
+                break;
+
+            case '5':
+                displayText.innerHTML += "5";
+                break;
+
+            case '6':
+                displayText.innerHTML += "6";
+                break;
+
+            case '7':
+                displayText.innerHTML += "7";
+                break;
+
+            case '8':
+                displayText.innerHTML += "8";
+                break;
+
+            case '9':
+                displayText.innerHTML += "9";
+                break;
+
+            case '0':
+                displayText.innerHTML += "0";
+                break;  
+
+            default:
+                break;
+        }
+    }
+    if (event.key == 'Backspace') {
+      displayText.innerHTML = displayText.innerHTML.slice(0, -1);
+    }
+})
